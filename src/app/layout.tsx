34d6a074ff95d3be3aefe57ab2866429d170c1dc
iamblackdev <wahabhabib23@gmail.com>
2024-12-27 15:39:35 +0100

    fix meta tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,8 @@ import './globals.css';
 export const metadata: Metadata = {
   title: 'Stecs',
   description: 'tecs Ethical Money App is The Ethical and Interest-Free Money App for Professionals. Save, invest and grow your wealth ethically with ease. Get Started. Track, save and build wealth.',
-  keywords: "money, bank app, finance, savings, credit, balance, savings, deposit, financial planning, investments, wealth management, digital banking, e-banking, online payments, savings account, financial services, credit cards, investment solutions, mobile banking, fintech, digital investing, fixed deposits, online transaction, dollar savings, vaults, banking vaults, savings vault, online investment, ethical, app, stecs, islamic, Islamic banking, Islamic Fintech, Sharia, ethical mobile money app, money app, Islamic Finance"
+  keywords: "money, bank app, finance, savings, credit, balance, savings, deposit, financial planning, investments, wealth management, digital banking, e-banking, online payments, savings account, financial services, credit cards, investment solutions, mobile banking, fintech, digital investing, fixed deposits, online transaction, dollar savings, vaults, banking vaults, savings vault, online investment, ethical, app, stecs, islamic, Islamic banking, Islamic Fintech, Sharia, ethical mobile money app, money app, Islamic Finance",
+
 };
 
 interface RootLayoutProps {
@@ -33,6 +34,22 @@ const RootLayout: FC<RootLayoutProps> = ({
   params: { locale },
 }) => (
   <html lang={locale}>
+    {/* Facebook Meta Tags */}
+    <meta property="og:url" content="https://stecs.ng/" />
+    <meta property="og:type" content="website" />
+    <meta property="og:title" content="Stecs Ethical Money App" />
+    <meta property="og:description"
+      content="Stecs Ethical Money App is The Ethical and Interest-Free Money App for Professionals. Save, invest and grow your wealth ethically with ease. Get Started. Track, save and build wealth." />
+    <meta property="og:image" content="" />
+
+    {/* Twitter Meta Tags */}
+    <meta name="twitter:card" content="summary_large_image" />
+    <meta property="twitter:domain" content="stecs.ng" />
+    <meta property="twitter:url" content="https://stecs.ng/" />
+    <meta name="twitter:title" content="Stecs Ethical Money App" />
+    <meta name="twitter:description" content="Stecs Ethical Money App" />
+    <meta name="twitter:image" content="" />
+
     <head>
       <ColorSchemeScript defaultColorScheme='auto' />
     </head>
