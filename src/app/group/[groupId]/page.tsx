db2c30837be31cb52eb90ee16a0f3f04525921a1
iamblackdev <wahabhabib23@gmail.com>
2025-09-01 10:11:37 +0100

    new update

--- src/app/group/[groupId]/page.tsx
+++ src/app/group/[groupId]/page.tsx
@@ -8,9 +8,20 @@ import SearchInput from '@/components/SearchInput';
 import { useRetrieveGroupQuery } from '@/config/api/client/slice';
 import { groupTableColumn } from '@/config/helpers/url';
 import { Loader } from '@mantine/core';
+import { useMemo, useState } from 'react';
 
 const GroupOverview = ({ params }: { params: { groupId: string } }): JSX.Element => {
 	const { data, isLoading: groupLoading } = useRetrieveGroupQuery(params?.groupId);
+
+	const [search, setSearch] = useState('');
+
+	const tableData = useMemo(() => {
+		console.log();
+		if (search.length >= 1) {
+			return data?.beneficiaries.filter((val) => val.firstName.toLowerCase().includes(search.toLowerCase()));
+		}
+		return data?.beneficiaries || [];
+	}, [data?.beneficiaries, search]);
 
 	if (groupLoading) {
 		return (
@@ -25,9 +36,9 @@ const GroupOverview = ({ params }: { params: { groupId: string } }): JSX.Element
 			<PaymentSummary data={data} />
 			<div className="bg-[#FFFFFF] mt-[32px] py-[32px] px-[19px] rounded-[8px]">
 				<div className="mb-[32px]">
-					<SearchInput placeholder="Search Recipient" />
+					<SearchInput placeholder="Search Recipient" onChange={(e) => setSearch(e.target.value)} />
 				</div>
-				<CustomTable columns={groupTableColumn} data={data?.beneficiaries || []} />
+				<CustomTable columns={groupTableColumn} data={tableData || []} />
 			</div>
 		</DashboardLayout>
 	);
