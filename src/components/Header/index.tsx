b71cd7bcf03cb84b43bcd4eceb99f88aeccbb3cb
iamblackdev <wahabhabib23@gmail.com>
2025-09-02 00:26:03 +0100

    new updates

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,6 @@
 'use client';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
-import HeaderLogo from '@/assets/icons/HeaderLogo';
 import { useAppDispatch, useTypedSelector } from '@/config/api/config/store';
 import { resetStore } from '@/config/api/auth/slice';
 import { getInitials } from '@/config/helpers/globals';
@@ -22,19 +21,22 @@ const AppHeader = () => {
 		<header className="w-full wrapper-pad px-[20px] md:px-[48px] h-[72px] flex items-center shadow-[0_1px_4px_rgba(0,0,0,0.1)]">
 			<div className="flex justify-between items-center w-full">
 				<Link href={user ? ROUTES.DASHBOARD : ROUTES.LOGIN}>
-					{/* <HeaderLogo /> */}
 					<Image src={appLogo} alt="" width={150} />
 				</Link>
 
-				{pathname !== '/' && (
-					<div className="flex items-center gap-[16px]">
-						<div className="bg-[#D9D9D9] rounded-full w-[48px] h-[48px] flex justify-center items-center">
-							<span className="text-[#003049] text-[20px] font-medium">{getInitials(user?.fullName)}</span>
-						</div>
-						<span onClick={handleLogOut} className="text-[crimson] font-semibold cursor-pointer">
-							Log Out
-						</span>
-					</div>
+				{pathname !== '/' && !pathname.includes('payout-details') && (
+					<>
+						{user && (
+							<div className="flex items-center gap-[16px]">
+								<div className="bg-[#D9D9D9] rounded-full w-[48px] h-[48px] flex justify-center items-center">
+									<span className="text-[#003049] text-[20px] font-medium">{getInitials(user?.fullName)}</span>
+								</div>
+								<span onClick={handleLogOut} className="text-[crimson] font-semibold cursor-pointer">
+									Log Out
+								</span>
+							</div>
+						)}
+					</>
 				)}
 			</div>
 		</header>
